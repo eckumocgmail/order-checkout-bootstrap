aceae067b04a1db8aa9c3b00d96a5540813f4485
unknown <eckumoc@gmail.com>
2022-07-15 04:52:18 +0300

    ыеуз щмук

--- obj/Debug/net5.0/RazorDeclaration/_Imports.razor.g.cs
+++ obj/Debug/net5.0/RazorDeclaration/_Imports.razor.g.cs
@@ -13,63 +13,63 @@ namespace bootstrap_checkout
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "D:\NetProjectsTemplates\bootstrap-checkout\_Imports.razor"
+#line 1 "D:\Projects\order-checkout-bootstrap\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "D:\NetProjectsTemplates\bootstrap-checkout\_Imports.razor"
+#line 2 "D:\Projects\order-checkout-bootstrap\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "D:\NetProjectsTemplates\bootstrap-checkout\_Imports.razor"
+#line 3 "D:\Projects\order-checkout-bootstrap\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "D:\NetProjectsTemplates\bootstrap-checkout\_Imports.razor"
+#line 4 "D:\Projects\order-checkout-bootstrap\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "D:\NetProjectsTemplates\bootstrap-checkout\_Imports.razor"
+#line 5 "D:\Projects\order-checkout-bootstrap\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "D:\NetProjectsTemplates\bootstrap-checkout\_Imports.razor"
+#line 6 "D:\Projects\order-checkout-bootstrap\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "D:\NetProjectsTemplates\bootstrap-checkout\_Imports.razor"
+#line 7 "D:\Projects\order-checkout-bootstrap\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "D:\NetProjectsTemplates\bootstrap-checkout\_Imports.razor"
+#line 8 "D:\Projects\order-checkout-bootstrap\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "D:\NetProjectsTemplates\bootstrap-checkout\_Imports.razor"
+#line 9 "D:\Projects\order-checkout-bootstrap\_Imports.razor"
 using bootstrap_checkout;
 
 #line default
